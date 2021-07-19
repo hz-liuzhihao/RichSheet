65eed303938dd9dfdf84fda768e5ce8f426dcc99
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-07-19 17:55:27 +0800

    feat: 增加设计层代码

--- src/controllers/InputListener.ts
+++ src/controllers/InputListener.ts
@@ -2,10 +2,11 @@ import { AbsListener, IListener } from './BehaviorListener';
 export default class InputListener extends AbsListener implements IListener {
 
   public dealClick(event: MouseEvent) {
-
+    const isDesign = this.excelBuild.isDesign();
+    
   }
 
   public dealDbClick(event: MouseEvent) {
-    
+    const isDesign = this.excelBuild.isDesign();
   }
 }
