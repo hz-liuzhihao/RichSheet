111c833fe08811054366071e0b85c8546564daa3
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-08-04 16:22:18 +0800

    feat: 修改编译模式增加样式

---
+++ build/webpack.example.pro.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, "../example/src/index.html"),
+  filename: "./index.html",
+});
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: "[name].css",
+  chunkFilename: "[id].css",
+});
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.join(__dirname, "../example/src/index.tsx"),
+  output: {
+    path: path.join(__dirname, "../example/dist"),
+    // 为从 entry 中配置生成的 Chunk 配置输出文件的名称
+    filename: "bundle.js",
+    // 为动态加载的 Chunk 配置输出文件的名称
+    chunkFilename: "[name].[hash].js",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "async",
+      minSize: 0,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        // pre/nomal/post - loader的执行顺序 - 前/中/后
+        enforce: "pre",
+        test: /\.tsx?/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|mp4)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 20,
+          },
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          "less-loader",
+        ],
+      },
+    ],
+  },
+  //处理路径解析
+  resolve: {
+    //extensions 拓展名
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".json"],
+    alias: {
+      custom: "../lib/index.js",
+    },
+  },
+  plugins: [htmlWebpackPlugin, miniCssExtractPlugin, new CleanWebpackPlugin()],
+};
