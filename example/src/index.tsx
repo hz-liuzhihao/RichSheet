a70f73dc382c15f2b9f6b38a5f51263a41c1439e
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-08-10 16:03:01 +0800

    feat: 增加字体大小功能

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -118,6 +118,24 @@ const FontSizeOption = [{
 }, {
   label: '32',
   value: 32
+}, {
+  label: '34',
+  value: 34
+}, {
+  label: '36',
+  value: 36
+}, {
+  label: '38',
+  value: 38
+}, {
+  label: '40',
+  value: 40
+}, {
+  label: '42',
+  value: 42
+}, {
+  label: '44',
+  value: 44
 }]
 
 interface CustomButtonArgs {
